4ee8f97742c8549c13e522816c1a1b71824faa66
oscar_gomez <oscar.gomez@ecole2600.com>
2022-10-27 15:23:24 +0200

    Added 4 sorting options, lastUpdated, name, status, maxBounty

--- README.md
+++ README.md
@@ -20,11 +20,17 @@ python main.py
 # Options
 
 ```
-usage: main.py [-h] [-j FILE]
+usage: main.py [-h] [-j FILE] [-s SORT_CODEs]
 
-InitigritiFetcher - Fetches all the programs and invites from Intigriti
+InitigritiFetcher  -  Fetches all the programs and invites from Intigriti
 
 options:
   -h, --help            show this help message and exit
   -j FILE, --json FILE  Save raw output in a json file
+  -s SORT_CODE(s), --sort SORT_CODE(s)
+                        Sort output using code(s):
+                        1: sort by last update
+                        2: sort by name
+                        3: sort by status
+                        4: sort by max bounty
 ```
--- main.py
+++ main.py
@@ -7,10 +7,51 @@ with open('config.json') as json_data_file:
 
 TOKEN = config['TOKEN']
 
-argparser = argparse.ArgumentParser(description='InitigritiFetcher  -  Fetches all the programs and invites from Intigriti')
+argparser = argparse.ArgumentParser(description='InitigritiFetcher  -  Fetches all the programs and invites from Intigriti', formatter_class=argparse.RawTextHelpFormatter)
 
 argparser.add_argument('-j', '--json', metavar="FILE", type=str, help='Save raw output in a json file', action='store')
+argparser.add_argument('-s', '--sort', metavar="SORT_CODE(s)", type=str, help="""Sort output using code(s):
+1: sort by last update
+2: sort by name
+3: sort by status
+4: sort by max bounty""", action='store')
 args = argparser.parse_args()
+
+def sort_by_last_update(list):
+    return list['lastUpdatedAt'] * -1
+
+def sort_by_name(list):
+    return list['name']
+
+def sort_by_status(list):
+    return list['status']
+
+def sort_by_max_bounty(list):
+    return list['maxBounty']["value"] * -1
+
+sort_funcs = []
+def setup():
+    global sort_funcs
+
+    if(TOKEN == "YOUR_TOKEN_HERE"):
+        print("Please configure the config.json file with your token")
+        exit()
+    
+    args.sort = args.sort.split(',')
+    for x in args.sort:
+        match x:
+            case '1':
+                sort_funcs.append(sort_by_last_update)
+            case '2':
+                sort_funcs.append(sort_by_name)
+            case '3':
+                sort_funcs.append(sort_by_status)
+            case '4':
+                sort_funcs.append(sort_by_max_bounty)
+            case _:
+                pass
+
+    main()
 
 def main():
     print('Getting the list of all the programs/invites')
@@ -52,6 +93,12 @@ def main():
     print("Programs: ")
 
     program_json = requests.get("https://api.intigriti.com/core/researcher/program", headers={"Authorization": "Bearer " + TOKEN}).json()
+
+    if(len(sort_funcs) > 0):
+        # sort by all sort_funs
+        program_json = sorted(program_json, key=lambda k: [ func(k) for func in sort_funcs ])
+    else:
+        program_json = sorted(program_json, key=sort_funcs[0])
 
     for program in program_json:
         total_json.append(program)
@@ -104,4 +151,4 @@ def main():
             json.dump(total_json, outfile)
 
 if __name__ == '__main__':
-    main()
+    setup()
